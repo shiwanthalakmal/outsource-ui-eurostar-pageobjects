4ebd2cbbb755edf172f30670fec49d9cf43be18c
shiwanthak <shiwanthak@zone24x7.com>
2016-04-19 21:47:36 +0530

    COM: basic page object package design

---
+++ src/main/java/com/outsource/qa/pages/RailplusHomePage.java
@@ -0,0 +1,11 @@
+package com.outsource.qa.pages;
+
+/**
+ * RailplusHomePage.java - RailplusHome page related page-objects definition class
+ *
+ * @author Shiwantha Lakmal
+ * @version 1.0-SNAPSHOT Last modified 04_20_2015
+ * @since 04/20/2015.
+ */
+public class RailplusHomePage {
+}
---
+++ src/main/java/com/outsource/qa/panels/RailplusHeaderPanel.java
@@ -0,0 +1,7 @@
+package com.outsource.qa.panels;
+
+/**
+ * Created by user on 4/18/2016.
+ */
+public class RailplusHeaderPanel {
+}
