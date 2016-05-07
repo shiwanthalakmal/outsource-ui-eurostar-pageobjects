56fcb77e9e642983a078d0e9e8721d11b4610734
shiwanthak <shiwanthak@zone24x7.com>
2016-05-07 19:08:06 +0530

    COM: Weekend mid shift

--- src/main/java/com/outsource/qa/pages/RailSearchResultPage.java
+++ src/main/java/com/outsource/qa/pages/RailSearchResultPage.java
@@ -4,6 +4,7 @@ import com.outsource.qa.panels.RailplusFooterPanel;
 import com.outsource.qa.panels.RailplusHeaderPanel;
 import com.outsource.qa.panels.TravelModalPanel;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
@@ -15,11 +16,23 @@ import org.openqa.selenium.support.PageFactory;
  */
 public class RailSearchResultPage extends BasicPage {
 
-    @FindBy(how= How.XPATH,using = "//*[@id='shop']/div[1]/h1")          private WebElement lblSearchResultTitle;
+    @FindBy(how= How.XPATH,using = "//*[@id='shop']/div[1]/h1")             private WebElement lblSearchResultTitle;
+    @FindBy(how= How.XPATH,using = "//*[@id='btnContinue']")                private WebElement btnShoppingCart;
+    @FindBy(how= How.XPATH,using = "html/body/div[3]/div[2]/div/h2")        private WebElement lblConditionOverlayTitle;
+    @FindBy(how= How.XPATH,using = "//div[@class='ui-dialog-buttonset']/button")                    private WebElement btnConditionClose;
+    @FindBy(how= How.XPATH,using = "//*[@id='departurePanel']/div[2]/div[5]/div[1]/div[4]/a[1]")    private WebElement btnJourneyDetails;
+    @FindBy(how= How.XPATH,using = "//span[contains(., 'Journey Details')]")                        private WebElement lblJourneyTitle;
+    @FindBy(how= How.XPATH,using = "html/body/div[3]/div[11]/div/button")                           private WebElement btnJourneyClose;
+    @FindBy(how= How.XPATH,using = "//*[@id='departurePanel']/div[2]/div[10]/input[1]")                           private WebElement btnEarlierSort;
+    @FindBy(how= How.XPATH,using = "//*[@id='departurePanel']/div[2]/div[10]/input[2]")                           private WebElement btnLaterSort;
+    @FindBy(how= How.XPATH,using = "//*[@id='departurePanel']/div[2]/div[5]/div[1]/div[3]/span[1]")                           private WebElement lblSortText;
 
     RailplusHeaderPanel headerPanel;
     RailplusFooterPanel footerPanel;
     TravelModalPanel searchPanel;
+
+    String total_cost = "";
+    String sort_before = "";
 
     public RailSearchResultPage(RemoteWebDriver driver) {
         super(driver);
@@ -33,5 +46,63 @@ public class RailSearchResultPage extends BasicPage {
         Assert.assertEquals((from+" to "+to), lblSearchResultTitle.getText());
         return this;
     }
+
+    public RailSearchResultPage step_Select_First_Class_Travel_Option(String option){
+        total_cost = driver.findElement(By.xpath("//*[@id='departurePanel']/div[2]/div[5]/div[2]//*[@id='fareOption_"+option+"']/span")).getText();
+        driver.findElement(By.xpath("//*[@id='departurePanel']/div[2]/div[5]/div[2]//*[@id='radio_"+option+"']")).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public RailSearchResultPage step_Select_Second_Class_Travel_Option(String option){
+        total_cost = total_cost + driver.findElement(By.xpath("//*[@id='departurePanel']/div[2]/div[5]/div[3]/div/div["+option+"]//span")).getText();
+        driver.findElement(By.xpath("//*[@id='departurePanel']/div[2]/div[5]/div[3]/div/div["+option+"]//input")).click();
+        return this;
+    }
+
+    public RailSearchResultPage check_And_Validate_Condition_Overlay(){
+        Assert.assertEquals("Conditions",lblConditionOverlayTitle.getText());
+        btnConditionClose.click();
+        return this;
+    }
+
+    public RailSearchResultPage step_Click_Journey_Details_Link(){
+        btnJourneyDetails.click();
+        return this;
+    }
+
+    public RailSearchResultPage check_And_Verify_Journey_Details_Title(){
+        Assert.assertEquals("Journey Details", lblJourneyTitle.getText());
+        return this;
+    }
+
+    public RailSearchResultPage check_And_Validate_Earlier_Sort_Functionality(){
+        sort_before = lblSortText.getText();
+        btnEarlierSort.click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue(!sort_before.equals(lblSortText.getText()));
+        return this;
+    }
+
+    public RailSearchResultPage check_And_Validate_Later_Sort_Functionality(){
+        sort_before = lblSortText.getText();
+        btnLaterSort.click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue(!sort_before.equals(lblSortText.getText()));
+        return this;
+    }
+
 
 }
